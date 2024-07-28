60b1537fd74ad38df3947ac8ea9963a6c40ab008
lab861265 <152097896+lab861265@users.noreply.github.com>
2024-07-29 00:16:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ import time
 import hashlib
 import re
 from datetime import datetime
-import threading
-
 
 from moviepy.editor import VideoFileClip
 
@@ -20,11 +18,8 @@ import platform
 import signal
 import shutil
 
-import http.client
-from urllib.parse import urlparse
-from tqdm import tqdm
 
-def convert_to_720p(input_path, need_credit, start_time=0, end_time=0):
+def convert_to_720p(input_path):
     clip = VideoFileClip(input_path)
     resolution = clip.size
     fps = clip.fps
@@ -45,17 +40,11 @@ def convert_to_720p(input_path, need_credit, start_time=0, end_time=0):
         '-i', renamed_path
     ]
     needPro = 0
-
-    if start_time != 0 or end_time != 0:
-        needPro = 1
-        ffmpeg_command.extend(['-ss', str(start_time)])
-        ffmpeg_command.extend(['-to', str(end_time)])
-
-    if resolution[0] >= 1920 and need_credit >= 300:
+    if resolution[0] > 720 or resolution[1] > 720:
         ffmpeg_command.append('-vf')
-        ffmpeg_command.append('scale=trunc(iw/4)*2:trunc(ih/4)*2')
+        ffmpeg_command.append('scale=trunc(iw/2)*2:trunc(ih/2)*2')
         needPro = 1
-    if resolution[1] >= 1920 and need_credit >= 300:
+    elif resolution[0] > 1280 or resolution[1] > 1280:
         ffmpeg_command.append('-vf')
         ffmpeg_command.append('scale=trunc(iw/4)*2:trunc(ih/4)*2')
         needPro = 1
@@ -76,7 +65,6 @@ def convert_to_720p(input_path, need_credit, start_time=0, end_time=0):
     #    needPro = 1
     ffmpeg_command.append(output_path)
 
-    needPro = 1
     if needPro:
         print(ffmpeg_command)
         #os.remove(output_path)
@@ -84,20 +72,17 @@ def convert_to_720p(input_path, need_credit, start_time=0, end_time=0):
         print(f"Video successfully converted. Saved at {output_path}")
     else:
         print(f"No need converted")
-        
-
-
 
 def calculate_md5(input_string):
     md5_hash = hashlib.md5(input_string.encode()).hexdigest()
     return md5_hash
-def upload_file_old(url, file_path, ext):
+def upload_file(url, file_path):
     chunk_size = 1024 * 1024 * 2  # 1MB
     total_chunks = -(-os.path.getsize(file_path) // chunk_size)  # 總分片數，無條件取整
     current_chunk = 0
     data = {'name': '', 'link': ''}
     fileSize = os.path.getsize(file_path)
-    upFileName = str(time.time()) + str(fileSize) + str(ext) + file_path
+    upFileName = str(time.time()) + str(fileSize) + file_path
     with open(file_path, 'rb') as f:
         while current_chunk < total_chunks:
             start = current_chunk * chunk_size
@@ -133,16 +118,6 @@ def upload_file_old(url, file_path, ext):
         print('Upload success!')
         return data
 
-def upload_file(file_path, ext=0):
-    
-    res = callApi("workerSignS3", {'filename': file_path})
-    print(res)
-    if res["code"] < 0:
-        print('sign s3url error')
-    upres = upload_file_to_s3(file_path, res["data"]["url"])
-    if upres:
-        return res["data"]["pubUrl"]
-    return False
 
 
 def download_file(url, filename):
@@ -170,108 +145,6 @@ def upload_image(upload_url, image_path):
             print(response_json)
     except Exception as e:
         print('Error:', str(e))
-
-
-def generate_video_thumbnail(video_path, thumbnail_path, max_size=512):
-    cap = cv2.VideoCapture(video_path)
-    ret, frame = cap.read()
-    
-    if ret:
-        # 获取视频帧的宽和高
-        frame_height, frame_width, _ = frame.shape
-        
-        # 调整缩略图的大小
-        if frame_width > max_size or frame_height > max_size:
-            if frame_width > frame_height:
-                new_width = max_size
-                new_height = int(frame_height * (max_size / frame_width))
-            else:
-                new_height = max_size
-                new_width = int(frame_width * (max_size / frame_height))
-            frame = cv2.resize(frame, (new_width, new_height))
-        
-        # 保存缩略图
-        cv2.imwrite(thumbnail_path, frame)
-    
-    cap.release()
-
-def calculate_md5(input_string):
-    md5_hash = hashlib.md5(input_string.encode()).hexdigest()
-    return md5_hash
-
-def upload_file_to_s3(file_path, signed_url):
-    """
-    Uploads a file to a signed S3 URL with progress information using http.client.
-
-    :param file_path: The path to the file to upload.
-    :param signed_url: The signed URL to upload the file to.
-    """
-    file_size = os.path.getsize(file_path)
-    url_parts = urlparse(signed_url)
-    conn = http.client.HTTPSConnection(url_parts.netloc)
-    
-    with open(file_path, 'rb') as file, tqdm(total=file_size, unit='B', unit_scale=True, desc=file_path) as pbar:
-        headers = {
-            'Content-Type': 'application/octet-stream',
-            'Content-Length': str(file_size),
-        }
-
-        conn.putrequest("PUT", url_parts.path + '?' + url_parts.query)
-        for key, value in headers.items():
-            conn.putheader(key, value)
-        conn.endheaders()
-
-        # Upload file in chunks
-        chunk_size = 1024 * 1024  # 1MB
-        while True:
-            data = file.read(chunk_size)
-            if not data:
-                break
-            conn.send(data)
-            pbar.update(len(data))
-
-    response = conn.getresponse()
-    print(response.status, response.reason)
-    if response.status == 200:
-        print("File uploaded successfully")
-        return True
-    else:
-        print("File upload failed")
-        return False
-
-
-
-def callApi(name, data):
-    try:
-        #TODO 做簽名認證
-        response = requests.post('https://api.fakeface.io/api/' + name, data, timeout=10)
-        response_json = response.json()
-        if response.status_code == 200:
-            print('Request successful')
-            return response_json
-        else:
-            print('Request failed')
-            print(response_json.get('message', 'Unknown error'))
-    except Exception as e:
-        print('Error:', str(e))
-
-def addLog(finish, state, log, process, total_frame = 0):
-    callApi("workerUpdateTask", {'task_id':taskData['_id'], 'total_frame':total_frame,'finish':finish, 'state':state, 'log':log, 'process':process})
-def gif2mp4(gif, mp4):
-    ffmpeg_command = [
-        'ffmpeg',
-        '-i', gif,
-        '-c:v', 'libx264',  # 使用H.264编码器
-        '-pix_fmt', 'yuv420p',  # 设置像素格式，通常需要
-        '-y',  # 强制覆盖
-        mp4
-    ]
-    ffmpeg_command = [
-        'convert',
-        gif,
-        mp4
-    ]
-    subprocess.run(ffmpeg_command)
 
 def add_border(input_image_path, output_image_path):
     try:
@@ -307,6 +180,63 @@ def add_border(input_image_path, output_image_path):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+
+def generate_video_thumbnail(video_path, thumbnail_path, max_size=512):
+    cap = cv2.VideoCapture(video_path)
+    ret, frame = cap.read()
+    
+    if ret:
+        # 获取视频帧的宽和高
+        frame_height, frame_width, _ = frame.shape
+        
+        # 调整缩略图的大小
+        if frame_width > max_size or frame_height > max_size:
+            if frame_width > frame_height:
+                new_width = max_size
+                new_height = int(frame_height * (max_size / frame_width))
+            else:
+                new_height = max_size
+                new_width = int(frame_width * (max_size / frame_height))
+            frame = cv2.resize(frame, (new_width, new_height))
+        
+        # 保存缩略图
+        cv2.imwrite(thumbnail_path, frame)
+    
+    cap.release()
+
+def calculate_md5(input_string):
+    md5_hash = hashlib.md5(input_string.encode()).hexdigest()
+    return md5_hash
+
+
+def callApi(name, data):
+    try:
+        #TODO 做簽名認證
+        response = requests.post('https://api.fakeface.io/api/' + name, data, timeout=10)
+        response_json = response.json()
+        if response.status_code == 200:
+            print('Request successful')
+            return response_json
+        else:
+            print('Request failed')
+            print(response_json.get('message', 'Unknown error'))
+    except Exception as e:
+        print('Error:', str(e))
+
+def addLog(finish, state, log, process, total_frame = 0):
+    callApi("workerUpdateTask", {'task_id':taskData['_id'], 'total_frame':total_frame,'finish':finish, 'state':state, 'log':log, 'process':process})
+def gif2mp4(gif, mp4):
+    ffmpeg_command = [
+        'ffmpeg',
+        '-i', gif,
+        '-c:v', 'libx264',  # 使用H.264编码器
+        '-pix_fmt', 'yuv420p',  # 设置像素格式，通常需要
+   #     '-vf', 'scale=trunc(iw/2)*2:trunc(ih/2)*2',  # 将高度和宽度调整为2的倍数
+        '-y',  # 强制覆盖
+        mp4
+    ]
+    subprocess.run(ffmpeg_command)
+  
     
 def mp42gif(input_mp4_filename, output_gif_filename):
     ffmpeg_command = [
@@ -318,21 +248,8 @@ def mp42gif(input_mp4_filename, output_gif_filename):
     ]
     subprocess.run(ffmpeg_command)
 
-def proc_media(media_filename, face_filename, out_file_path, is_enhancement, need_credit):
+def proc_media(media_filename, face_filename, out_file_path, is_enhancement, reference_frame_number):
     print(media_filename, face_filename, out_file_path)
-
-    #clip = VideoFileClip(media_filename)
-    #duration = clip.duration
-
-    outTime = need_credit * 30
-
-    #if media_filename.lower().endswith(('.jpg')):
-    #    outTime = 200
-
-    #if outTime > 3600:
-    #    outTime = 7200
-
-
     #python run.py -o ./out.mp4 -s face.jpg -t media.mp4 --frame-processors face_swapper  --headless  --execution-providers coreml
     mode = 'cuda'
     if sys.argv[1] == 'cpu':
@@ -351,27 +268,39 @@ def proc_media(media_filename, face_filename, out_file_path, is_enhancement, nee
         '--face-mask-types','occlusion',
         #'--reference-frame-number', reference_frame_number,
         #'--reference-face-distance','1',
-        '--execution-thread-count', '32',
-        '--execution-queue-count','2',
-        '--video-memory-strategy','tolerant',
-        '--temp-frame-format','bmp',
-        '--output-video-quality','70',
-        '--output-video-preset','ultrafast',
         '--face-detector-score','0.25',
+        '--output-video-quality','70',
         '--frame-processors','face_swapper'
     ]
     if is_enhancement:
         command.append('face_enhancer')
         
-   # subprocess.run(command)
-
-    try:
-        result = subprocess.run(command, timeout=outTime, check=True, stdout=subprocess.PIPE)
-    except subprocess.TimeoutExpired:
-        print('执行命令超时')
-
+    subprocess.run(command)
     return
-    
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True  # 用于以文本模式获取输出
+    )
+
+    while True:
+        output_line = process.stdout.readline()
+        if not output_line and process.poll() is not None:
+            break
+
+        # 在这里解析输出并提取进度信息
+        progress_match = re.search(r'Processing:\s+(\d+)%', output_line)
+        if progress_match:
+            progress_percentage = int(progress_match.group(1))
+            print(f'Progress: {progress_percentage}%')
+
+        # 如果你还想要其他输出，可以在这里处理
+
+        time.sleep(1)
+
+    process.stdout.close()
+    process.wait() 
 
 
 def delete_files(file_paths):
@@ -383,10 +312,6 @@ def delete_files(file_paths):
             print(f"文件 '{file_path}' 不存在，无需删除。")
    
 def work():
-
-#    res = upload_file('./test.jpg');
-#    print(res)
-#    return
     global taskData
     mode = 'cuda'
     if sys.argv[1] == 'cpu':
@@ -433,38 +358,29 @@ def work():
     extName = os.path.splitext(media_file_url)[1].lower()
 
     is_enhancement = int(taskData.get('is_enhancement', 0))
-    need_credit = int(taskData.get('need_credit', 0))
-
-    start_time = int(taskData.get('start_time', 0))
-    end_time = int(taskData.get('end_time', 0))
-
-    part = taskData.get('part', '')
-
     reference_frame_number = str(taskData.get('reference_frame_number', 0))
 
-    print('is_enhancement, need_credit', is_enhancement, need_credit);
+    print('is_enhancement, reference_frame_number', is_enhancement, reference_frame_number);
         
     if media_filename.lower().endswith(('.mp4', '.m4v', '.mkv', '.avi', '.mov', '.webm', '.mpeg', '.mpg', '.wmv', '.flv', '.asf', '.3gp', '.3g2', '.ogg', '.vob', '.rmvb', '.ts', '.m2ts', '.divx', '.xvid', '.h264', '.avc', '.hevc', '.vp9', '.avchd')):
         
-        try:
-    	    convert_to_720p(media_filename, need_credit, start_time, end_time)
-        except Exception as e:
-            print(e)
+        convert_to_720p(media_filename)
+
         media_filename = 'media.mp4'
         
         out_file_path = 'media_out.mp4'
-        proc_media(media_filename, face_filename, out_file_path, is_enhancement, need_credit)
+        proc_media(media_filename, face_filename, out_file_path, is_enhancement, reference_frame_number)
         thumb_file_path = 'thumb_media.jpg'
         generate_video_thumbnail(out_file_path, thumb_file_path)
         if not os.path.exists(out_file_path):
             print(f"找不到文件 {out_file_path}")
             addLog(1, -1, 'Processing failed', 99)
             return
-        upload_video_url = upload_file(out_file_path)
-        upload_image_url = upload_file(thumb_file_path)
-        print('Upload result:', upload_video_url, upload_image_url)
+        upload_video_res = upload_file('https://fakeface.io/upload.php?m=media', out_file_path)
+        upload_image_res = upload_image('https://fakeface.io/upload.php?m=thumb', thumb_file_path)
+        print('Upload result:', upload_video_res, upload_image_res)
         now = datetime.now()
-        api_res = callApi("wokerAddMedia", {'user_id':data['data']['user_id'], 'media_id':data['data']['finish_media_id'], 'file_url':upload_video_url, 'thumb_url':upload_image_url, 'file_hash':now.strftime("%Y-%m-%d %H:%M:%S")})
+        api_res = callApi("wokerAddMedia", {'user_id':data['data']['user_id'], 'media_id':data['data']['finish_media_id'], 'file_url':upload_video_res['link'], 'thumb_url':upload_image_res['thumb'], 'file_hash':now.strftime("%Y-%m-%d %H:%M:%S")})
         print('Api result:', api_res)
         addLog(1, 3, 'finish', 100)
         return
@@ -473,7 +389,7 @@ def work():
         out_file_path = 'media_out.mp4'
         print('文件后缀：', extName)
         gif2mp4('media.gif', 'media.mp4')
-        proc_media('media.mp4', face_filename, out_file_path, is_enhancement,need_credit)
+        proc_media('media.mp4', face_filename, out_file_path, is_enhancement,reference_frame_number)
         thumb_file_path = 'thumb_media.jpg'
         generate_video_thumbnail(out_file_path, thumb_file_path)
         mp42gif('media_out.mp4', 'media_out.gif')
@@ -482,31 +398,29 @@ def work():
             print(f"找不到文件 {out_file_path}")
             addLog(1, -1, 'Processing failed', 99)
             return
-        upload_video_url = upload_file(out_file_path)
-        upload_image_url = upload_file(thumb_file_path)
-        print('Upload result:', upload_video_url, upload_image_url)
+        upload_video_res = upload_file('https://fakeface.io/upload.php?m=media', out_file_path)
+        upload_image_res = upload_image('https://fakeface.io/upload.php?m=thumb', thumb_file_path)
+        print('Upload result:', upload_video_res, upload_image_res)
         now = datetime.now()
-        api_res = callApi("wokerAddMedia", {'user_id':data['data']['user_id'], 'media_id':data['data']['finish_media_id'], 'file_url':upload_video_url, 'thumb_url':upload_image_url, 'file_hash':now.strftime("%Y-%m-%d %H:%M:%S")})
+        api_res = callApi("wokerAddMedia", {'user_id':data['data']['user_id'], 'media_id':data['data']['finish_media_id'], 'file_url':upload_video_res['link'], 'thumb_url':upload_image_res['thumb'], 'file_hash':now.strftime("%Y-%m-%d %H:%M:%S")})
         print('Api result:', api_res)
         addLog(1, 3, 'finish', 100)
         return
-    if media_filename.lower().endswith(('.jpg')) or media_filename.lower().endswith(('.webp')):
+    if media_filename.lower().endswith(('.jpg')):
         out_file_path = 'media_out.jpg'
         real_out_file_path = 'media_out.jpg'
-        proc_media(media_filename, face_filename, out_file_path, 1,need_credit)
-        thumb_file_path = 'thumb_media.jpg'
-        generate_img_thumbnail(out_file_path, thumb_file_path)
+        proc_media(media_filename, face_filename, out_file_path, 1,reference_frame_number)
+
         if not os.path.exists(out_file_path):
             print(f"找不到文件 {out_file_path}")
             addLog(1, -1, 'Processing failed', 99)
             return
-        addLog(0, 2, 'finish quickly', 99)
-        upload_file_url = upload_file(out_file_path)
-        upload_image_url = upload_file(thumb_file_path)
+     #   addLog(0, 2, 'finish quickly', 99)
+        upload_res = upload_image('https://fakeface.io/upload.php?m=png', out_file_path)
         now = datetime.now()
 
         print('Upload result:', upload_res)
-        api_res = callApi("wokerAddMedia", {'user_id':data['data']['user_id'], 'media_id':data['data']['finish_media_id'], 'file_url':upload_file_url, 'thumb_url':upload_image_url, 'file_hash':now.strftime("%Y-%m-%d %H:%M:%S") })
+        api_res = callApi("wokerAddMedia", {'user_id':data['data']['user_id'], 'media_id':data['data']['finish_media_id'], 'file_url':upload_res['link'], 'thumb_url':upload_res['thumb'], 'file_hash':now.strftime("%Y-%m-%d %H:%M:%S") })
         print('Api result:', api_res)
         addLog(1, 3, 'finish', 100)
         return
